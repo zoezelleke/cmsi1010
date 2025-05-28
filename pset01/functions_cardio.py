156569be5b1355e6b5f3e29c9f35c94438b77b58
Zoe <zzelleke@lion.lmu.edu>
2025-05-28 12:50:34 -0700

    added problem set 1

---
+++ pset01/functions_cardio.py
@@ -0,0 +1,82 @@
+# ----------------------------------------------------------------------
+# This is the file functions_cardio.py
+
+# The intent is to give you practice writing functions.
+
+# Complete the functions below.
+# Remove this comment when you have completed the functions.
+# ----------------------------------------------------------------------
+
+def is_odd(n):
+    """
+    Returns True if n is odd, False otherwise.
+    """
+    # replace the pass statement with your code
+    pass
+
+
+def median_of_three(a, b, c):
+    """
+    Returns the median of three numbers a, b, and c.
+    """
+    # replace the pass statement with your code
+    pass
+
+
+def is_palindrome(s):
+    """
+    Returns True if the string s is a palindrome, False otherwise.
+
+    A palindrome reads the same forwards and backwards. You can
+    implement it as a simple check to see if s is equal to its
+    reversal.
+    """
+    # replace the pass statement with your code
+    pass
+
+
+def factorial(n):
+    """
+    Returns the factorial of n (n!).
+
+    The factorial of a non-negative integer n is the product of all
+    positive integers less than or equal to n. Please implement this
+    function with a for loop.
+    """
+    # replace the pass statement with your code
+    pass
+
+
+def count_of_latin_vowels(s):
+    """
+    Returns the number of vowels in the string s.
+
+    The vowels are 'a', 'e', 'i', 'o', and 'u'. You can implement this
+    function using a for loop to iterate through the string.
+    """
+    # replace the pass statement with your code
+    pass
+
+
+def longest_string(strings):
+    """
+    Returns the longest string from a list of strings.
+
+    If there are multiple strings with the same maximum length, return
+    the first one encountered.
+    """
+    # replace the pass statement with your code
+    pass
+
+
+def word_frequencies(s):
+    """
+    Returns a dictionary with the frequency of each word in the string s.
+    The keys of the dictionary are the words, and the values are the
+    number of times each word appears in the string.
+
+    A word is defined as a sequence of characters separated by spaces.
+    You can implement this function using the split method.
+    """
+    # replace the pass statement with your code
+    pass
---
+++ pset01/number_guesser.py
@@ -0,0 +1,34 @@
+# ----------------------------------------------------------------------
+# This is the file number_guesser.py
+
+# The intent is to give you practice writing a complete, interactive
+# Python program.
+
+# Remove the comments in this file when you have completed your program.
+# You can, and should, include your own comments, but please remove the
+# comments that are here now.
+# ----------------------------------------------------------------------
+
+# Things to do:
+
+# Generate a random number between 1 and 1000.
+
+# Ask the user to guess the number. In your prompt, let the user know they
+# can type 'bye' or 'exit' to quit the program.
+#
+# If their guess is not made up entirely of digits, print "Please enter a valid
+# number" and ask them to guess again.
+#
+# If the guess is too high, print "Too high!" and continue asking.
+#
+# If the guess is too low, print "Too low!" and continue asking.
+#
+# If the guess is correct, print "Congratulations! You guessed the number!" along
+# with the number of attempts it took to guess the number. Start over with a new
+# random number. Make sure to zero out the number of attempts.
+
+# Please note: There are likely to be a number of Python guessing games online,
+# and most GenAI systems can probably write this for you. Don’t rely on them,
+# as they rob you of a chance to practice your Python skills and they might not
+# even be correct. Perhaps, worse, they might not follow the instructions
+# exactly as given.
