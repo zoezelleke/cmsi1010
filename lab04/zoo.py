4cc4893a6f11602c254aa00714ad33980a1d08a5
Zoe <zzelleke@lion.lmu.edu>
2025-05-28 14:02:25 -0700

    added lab04

---
+++ lab04/zoo.py
@@ -0,0 +1,44 @@
+def show_help():
+    print("Type 'help' to get a list of all the things you can do")
+    print("Type 'see' to get a list of all the animals")
+    print("Type 'pet' followed by the animal's name to pet a particular animal")
+    print("Type 'bye' to leave the zoo and exit the program")
+
+
+def show_all_animals():
+    print("The animals in the zoo are:")
+    print("• Clover the Bunny 🐇")
+    print("• Coco the Baby Goat 🐐")
+    print("• Arno the Alligator 🐊")
+
+
+def pet_animal(animal):
+    if animal == "Clover":
+        print("Clover is so happy! ❤️")
+    elif animal == "Coco":
+        print("Coco the Baby Goat thanks you! 🥰")
+    elif animal == "Arno":
+        print("Actually, we cannot allow you to pet Arno. ⛔️")
+    else:
+        print("Sorry, I don't know that animal")
+
+
+print("Welcome to the Petting Zoo!")
+print("Type 'help' to get a list of all the things you can do")
+print()
+
+keep_going = True
+while keep_going:
+    response = input("What would you like to do? ")
+    if response == "help": 
+        show_help()
+    elif response == "see":
+        show_all_animals()
+    elif response.startswith("pet "):
+        animal = response[4:]
+        pet_animal(animal)
+    elif response == "bye":
+        print("Goodbye!")
+        keep_going = False
+    else:
+        print("Sorry, I don't understand that command")
